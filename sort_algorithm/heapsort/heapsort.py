297a5901bf6698f7edb1929053d6aded3691e48d
junfeng-pc <840282629@qq.com>
2018-03-15 12:01:35 +0800

    修改了交换两个元素的方法，使代码更简洁

--- sort_algorithm/heapsort/heapsort.py
+++ sort_algorithm/heapsort/heapsort.py
@@ -11,7 +11,8 @@ class Heap():
     RIGHT = lambda self, i: (i << 1) + 1
     PARENT = lambda self, i: i >> 1
     INFINITE_SMALL = -10001
-    INFINITE_LARGE=10001
+    INFINITE_LARGE = 10001
+
     def __init__(self, A):
         self.heap_size = 0
         self.A = A[:]
@@ -21,9 +22,7 @@ class Heap():
         self.build_max_heap()
         self.heap_size
         for i in range(len(self.A), 1, -1):  # 从len(B)到2
-            temp = self.A[0]  # 交换第1个元素和第i个元素
-            self.A[0] = self.A[i - 1]
-            self.A[i - 1] = temp
+            self.A[0], self.A[i - 1] = self.A[i - 1], self.A[0]  # 交换第1个元素和第i个元素
             self.heap_size -= 1
             self.max_heaplify(1)
         return self.A
@@ -38,9 +37,7 @@ class Heap():
         if R <= self.heap_size and self.A[R - 1] > self.A[largest - 1]:
             largest = R
         if largest != i:
-            temp = self.A[i - 1]
-            self.A[i - 1] = self.A[largest - 1]
-            self.A[largest - 1] = temp
+            self.A[i - 1], self.A[largest - 1] = self.A[largest - 1], self.A[i - 1]
             self.max_heaplify(largest)
 
     def min_heaplify(self, i):
@@ -53,9 +50,7 @@ class Heap():
         if R <= self.heap_size and self.A[R - 1] < self.A[smallest - 1]:
             smallest = R
         if smallest != i:
-            temp = self.A[i - 1]
-            self.A[i - 1] = self.A[smallest - 1]
-            self.A[smallest - 1] = temp
+            self.A[i - 1], self.A[smallest - 1] = self.A[smallest - 1], self.A[i - 1]
             self.min_heaplify(smallest)
 
     def build_max_heap(self):
@@ -101,9 +96,7 @@ class Heap():
             raise Exception('new key is smaller than current key')
         self.A[i - 1] = key
         while i > 1 and self.A[self.PARENT(i) - 1] < self.A[i - 1]:
-            temp = self.A[i - 1]
-            self.A[i - 1] = self.A[self.PARENT(i) - 1]
-            self.A[self.PARENT(i) - 1] = temp
+            self.A[i - 1], self.A[self.PARENT(i) - 1] = self.A[self.PARENT(i) - 1], self.A[i - 1]
             i = self.PARENT(i)
         return self.A
 
@@ -112,9 +105,7 @@ class Heap():
             raise Exception('new key is larger than current key')
         self.A[i - 1] = key
         while i > 1 and self.A[self.PARENT(i) - 1] > self.A[i - 1]:
-            temp = self.A[i - 1]
-            self.A[i - 1] = self.A[self.PARENT(i) - 1]
-            self.A[self.PARENT(i) - 1] = temp
+            self.A[i - 1], self.A[self.PARENT(i) - 1] = self.A[self.PARENT(i) - 1], self.A[i - 1]
             i = self.PARENT(i)
         return self.A
 
@@ -130,15 +121,15 @@ class Heap():
         self.heap_decrease_key(self.heap_size, key)
         return self.A
 
-    def max_heap_delete(self,i):
-        maximum=self.heap_maximum()
-        self.heap_increase_key(i,maximum+1)
+    def max_heap_delete(self, i):
+        maximum = self.heap_maximum()
+        self.heap_increase_key(i, maximum + 1)
         self.heap_extract_max()
         return self.A
 
-    def min_heap_delete(self,i):
-        minimum=self.heap_maximum()
-        self.heap_decrease_key(i,minimum-1)
+    def min_heap_delete(self, i):
+        minimum = self.heap_maximum()
+        self.heap_decrease_key(i, minimum - 1)
         self.heap_extract_min()
         return self.A
 
@@ -155,8 +146,9 @@ def heap_test():
     print "取出最大元素后的堆:", heap.get_heap()
     print "插入元素16后的堆", heap.max_heap_insert(16)
     print "将第二个元素提升到17:", heap.heap_increase_key(2, 17)
-    print "删除第2个元素",heap.max_heap_delete(2)
+    print "删除第2个元素", heap.max_heap_delete(2)
     print "堆排序", heap.heapsort()
+
 
 def heap_test2():
     data = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
@@ -167,8 +159,7 @@ def heap_test2():
     print "取出最小元素后的堆:", heap.get_heap()
     print "插入元素5后的堆", heap.min_heap_insert(5)
     print "将第二个元素降低到-1:", heap.heap_decrease_key(2, -1)
-    print "删除第2个元素",heap.min_heap_delete(2)
-
+    print "删除第2个元素", heap.min_heap_delete(2)
 
 
 def sort_test():
--- sort_algorithm/quicksort/quicksort.py
+++ sort_algorithm/quicksort/quicksort.py
@@ -6,6 +6,7 @@ from test_data.create_random_data import read_random_data
 from test_data.create_random_data import save_sorted
 import random
 
+
 class Quicksort(object):
     @log_time
     def quicksort(self, A):
@@ -25,37 +26,48 @@ class Quicksort(object):
         for j in range(p, r):  # 从p ,p+1,...,r-1
             if A[j] <= x:
                 i += 1  # 边界扩张
-                temp = A[j]  # 交换A[i]与A[j]
-                A[j] = A[i]
-                A[i] = temp
-        temp = A[i + 1]  # 交换pivot到中心
-        A[i + 1] = A[r]
-        A[r] = temp
+                A[i], A[j] = A[j], A[i]  # 交换A[i]与A[j]
+        # 交换pivot到中心
+        A[r], A[i + 1] = A[i + 1], A[r]
         return i + 1
 
+
 class Randomized_quicksort(Quicksort):
-    #重写sort,调用randomize_partition实现随机化快排
-    def sort(self,A,p,r):
+    # 重写sort,调用randomize_partition实现随机化快排
+    def sort(self, A, p, r):
         if p < r:
             q = self.randomize_partition(A, p, r)
             self.sort(A, p, q - 1)
             self.sort(A, q + 1, r)
             return A
-    def randomize_partition(self,A,p,r):
-        i=random.randint(p,r)  #随机选出pivot
-        temp=A[r]             #交换pivot至末尾 复用子类 partition
-        A[r]=A[i]
-        A[i]=temp
-        return self.partition(A,p,r)
+
+    def randomize_partition(self, A, p, r):
+        i = random.randint(p, r)  # 随机选出pivot
+        # 交换pivot至末尾 复用子类 partition
+        A[r],A[i]=A[i],A[r]
+        return self.partition(A, p, r)
+
+
+@log_time
+def list_sort(B):
+    A = B[:]
+    A.sort()
+    return A
+
 
 if __name__ == '__main__':
     numbers = 100000
-    create_random_data(numbers)
-    origin_data = read_random_data(numbers)
+    # create_random_data(numbers)
+    # origin_data = read_random_data(numbers)
+    origin_data = []
+    for _ in range(0, numbers):
+        origin_data.append(random.randint(-10000, 10000))
     quick = Quicksort()
-    r_quick=Quicksort()
-    #sorted_data = quick.quicksort(origin_data)
-    r_sorted_data=r_quick.quicksort(origin_data)
-    save_sorted(r_sorted_data)
+    r_quick = Quicksort()
+    # sorted_data = quick.quicksort(origin_data)
+    r_sorted_data = r_quick.quicksort(origin_data)
+    l_sorted_data = list_sort(origin_data)
+    # save_sorted(r_sorted_data)
     print "排序前(前10个数据):\n", origin_data[:10]
     print "排序后(前10个数据):\n", r_sorted_data[:10]
+    print "list排序后(前10个数据):\n", l_sorted_data[:10]
